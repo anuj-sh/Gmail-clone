8771256a7f78174673d27e9b54c5e5e9482a6b28
anuj-sh <129144463+anuj-sh@users.noreply.github.com>
2024-07-26 21:57:21 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import emailRoute from "./routes/email.route.js";
 
 dotenv.config({});
 connectDB();
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
 const app = express();
 
 // middleware
