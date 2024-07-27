7fce6d0fb35da1a007c9a12f421cb518f2caa25f
anuj-sh <129144463+anuj-sh@users.noreply.github.com>
2024-07-27 17:05:29 +0530

    Update Mail.jsx

--- frontend/src/components/Mail.jsx
+++ frontend/src/components/Mail.jsx
@@ -15,7 +15,7 @@ const Mail = () => {
     const params = useParams();
     const deleteHandler = async () => {
         try {
-            const res = await axios.delete(`http://localhost:8080/api/v1/email/${params.id}`,{withCredentials:true});
+            const res = await axios.delete(`https://gmail-clone-frontend.onrender.com/api/v1/email/${params.id}`,{withCredentials:true});
             toast.success(res.data.message);
             navigate("/");
         } catch (error) {
