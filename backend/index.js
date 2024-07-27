692944f0e52cdcf7b12bcbef0ed4fce5fbcaae53
anuj-sh <129144463+anuj-sh@users.noreply.github.com>
2024-07-27 15:52:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://gmail-clone-frontend.onrender.com',
     credentials:true
 }
 app.use(cors(corsOptions));
