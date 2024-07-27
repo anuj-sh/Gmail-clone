8e8b9694d051bbc48502e1420bf3888b66331f20
anuj-sh <129144463+anuj-sh@users.noreply.github.com>
2024-07-27 16:13:16 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -19,7 +19,7 @@ const Signup = () => {
     const submitHandler = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post("https://gmail-clone-uk0w.onrender.com/api/v1/auth/signup", input, {
+            const res = await axios.post("https://gmail-clone-uk0w.onrender.com/api/v1/auth/register", input, {
                 headers:{
                     'Content-Type':"application/json"
                 },
